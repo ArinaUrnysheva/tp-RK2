4a549fa58e61d73ee729d0dd7bdef8351cbef091
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-05-16 16:06:08 +0300

    Update FlyweightTest.cpp

--- tests/FlyweightTest.cpp
+++ tests/FlyweightTest.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 #include "../flyweight.cpp"
 
+/*
 TEST(FlyweightTest, FlyweightFactoryWorks) {
   jc::FlyweightFactory factory;
 
@@ -29,6 +30,53 @@ TEST(FlyweightTest, FlyweightFactoryWorks) {
   flyweight_ptr3->Print();
   std::string output3 = testing::internal::GetCapturedStdout();
   EXPECT_EQ(output3, std::string(1, another_char));
+}
+*/
+
+TEST(FlyweightTest, CreateNewFlyweight) {
+  jc::FlyweightFactory factory;
+
+  // Request a Flyweight for a new character
+  char new_char = 'A';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr1 = factory.FlyweightPtr(new_char);
+  EXPECT_NE(flyweight_ptr1, nullptr);
+
+  // Check if the Flyweight is correctly created
+  testing::internal::CaptureStdout();
+  flyweight_ptr1->Print();
+  std::string output1 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output1, std::string(1, new_char));
+}
+
+TEST(FlyweightTest, ReuseExistingFlyweight) {
+  jc::FlyweightFactory factory;
+
+  // Request a Flyweight for a new character
+  char new_char = 'A';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr1 = factory.FlyweightPtr(new_char);
+
+  // Request a Flyweight for the same character again
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr2 = factory.FlyweightPtr(new_char);
+  EXPECT_EQ(flyweight_ptr1, flyweight_ptr2);
+}
+
+TEST(FlyweightTest, CreateDifferentFlyweights) {
+  jc::FlyweightFactory factory;
+
+  // Request a Flyweight for a new character
+  char new_char = 'A';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr1 = factory.FlyweightPtr(new_char);
+
+  // Request a Flyweight for another character
+  char another_char = 'B';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr2 = factory.FlyweightPtr(another_char);
+  EXPECT_NE(flyweight_ptr1, flyweight_ptr2);
+
+  // Check if the Flyweights are correctly created
+  testing::internal::CaptureStdout();
+  flyweight_ptr2->Print();
+  std::string output2 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output2, std::string(1, another_char));
 }
 
 int main(int argc, char **argv) {
