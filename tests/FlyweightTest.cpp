a6731ac901d2330cf38d72f6a0177828475b22fd
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-05-16 15:59:16 +0300

    Update FlyweightTest.cpp

--- tests/FlyweightTest.cpp
+++ tests/FlyweightTest.cpp
@@ -1,21 +1,37 @@
 #include <gtest/gtest.h>
-#include "../flyweight/Flyweight.cpp"  // Include the implementation file for testing
+#include "../flyweight.cpp"
 
-// Test the FlyweightFactory class
-TEST(FlyweightFactoryTest, FlyweightCreation) {
+TEST(FlyweightTest, FlyweightFactoryWorks) {
   jc::FlyweightFactory factory;
 
-  // Test creating and printing flyweight objects
-  EXPECT_NO_THROW({
-    EXPECT_NO_THROW(factory.FlyweightPtr('a')->Print());
-    EXPECT_NO_THROW(factory.FlyweightPtr('a')->Print());
-    EXPECT_NO_THROW(factory.FlyweightPtr('b')->Print());
-    EXPECT_NO_THROW(factory.FlyweightPtr('b')->Print());
-  });
+  // Request a Flyweight for a new character
+  char new_char = 'A';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr1 = factory.FlyweightPtr(new_char);
+  EXPECT_NE(flyweight_ptr1, nullptr);
+
+  // Check if the Flyweight is correctly created
+  testing::internal::CaptureStdout();
+  flyweight_ptr1->Print();
+  std::string output1 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output1, std::string(1, new_char));
+
+  // Request a Flyweight for an existing character
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr2 = factory.FlyweightPtr(new_char);
+  EXPECT_EQ(flyweight_ptr1, flyweight_ptr2);
+
+  // Request a Flyweight for another character
+  char another_char = 'B';
+  std::unique_ptr<jc::Flyweight>& flyweight_ptr3 = factory.FlyweightPtr(another_char);
+  EXPECT_NE(flyweight_ptr3, nullptr);
+
+  // Check if it was correctly created
+  testing::internal::CaptureStdout();
+  flyweight_ptr3->Print();
+  std::string output3 = testing::internal::GetCapturedStdout();
+  EXPECT_EQ(output3, std::string(1, another_char));
 }
 
-/* Run all tests
-int main(int argc, char** argv) {
-  testing::InitGoogleTest(&argc, argv);
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
-}*/
+}
