b9613f2051f76915eab500853a43a279011c6e40
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-05-16 11:53:48 +0300

    Add files via upload

---
+++ flyweight.hpp
@@ -0,0 +1,36 @@
+#ifndef FLYWEIGHT_HPP
+#define FLYWEIGHT_HPP
+
+#include <iostream>
+#include <memory>
+#include <unordered_map>
+#include <utility>
+
+namespace jc {
+
+	class Flyweight {
+	public:
+		virtual void Print() = 0;
+		virtual ~Flyweight() = default;
+	};
+
+	class ConcreteFlyweight : public Flyweight {
+	public:
+		explicit ConcreteFlyweight(char key);
+		void Print() override;
+
+	private:
+		char key_;
+	};
+
+	class FlyweightFactory {
+	public:
+		std::unique_ptr<Flyweight>& FlyweightPtr(char c);
+
+	private:
+		std::unordered_map<char, std::unique_ptr<Flyweight>> m_;
+	};
+
+}  // namespace jc
+
+#endif // FLYWEIGHT_HPP
