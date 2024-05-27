945d304b4463b56f3b66a26e8a6578546ce42cb2
ArinaUrnysheva <148492062+ArinaUrnysheva@users.noreply.github.com>
2024-05-27 13:52:41 +0300

    Create main.cpp

---
+++ demo/main.cpp
@@ -0,0 +1,9 @@
+#include "../flyweight.cpp"
+
+int main() {
+  jc::FlyweightFactory factory;
+  factory.FlyweightPtr('a')->Print();  // a
+  factory.FlyweightPtr('a')->Print();  // existing key: a
+  factory.FlyweightPtr('b')->Print();  // b
+  factory.FlyweightPtr('b')->Print();  // existing key: a
+}
